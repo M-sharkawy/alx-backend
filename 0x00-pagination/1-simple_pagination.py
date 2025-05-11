6c31bb6cf6a9ab47fbcf0417bf75a322064f072e
M-sharkawy <mostafasharks021@gmail.com>
2025-05-12 02:48:24 +0300

    task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+module to Implement a method named get_page
+in the provided class for pagination
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int):
+    """
+    function that return the start index and an end index
+    page : int number of page
+    page_size : int page size content
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """pagination methods of pages"""
+        assert isinstance(page, int) and page > 0, \
+            "page must be greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "page_size must be greater than 0"
+
+        data = self.__dataset
+        start, end = index_range(page, page_size)
+        if start >= len(data):
+            return []
+
+        return data[start:end]
